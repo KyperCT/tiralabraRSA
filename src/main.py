e49c33f6f9ed80416ac8e3da6b36d2e12caed828
inanimateobject <mirko.vonbell@helsinki.fi>
2022-01-25 03:51:04 +0200

    Built up interface, added manual test folder

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import argparse
 import rsa
 
+
 def main():
     parser = argparse.ArgumentParser(description='Tool for RSA keygen and encrypt/decrypt')
     options = parser.add_mutually_exclusive_group()
@@ -9,7 +10,26 @@ def main():
     options.add_argument('--generate', action='store_true', help='No arguments, creates keys')
 
     args = parser.parse_args()
-    print(args)
+
+    out = None
+
+    if args.encrypt is not None:
+        with open(args.encrypt[0], "r") as f:
+            txt = f.read()
+        with open(args.encrypt[1], "r") as f:
+            key = f.read()
+        out = rsa.encrypt(txt, key)
+    if args.decrypt is not None:
+        with open(args.decrypt[0], "r") as f:
+            cipher = f.read()
+        with open(args.decrypt[1], "r") as f:
+            key = f.read()
+        out = rsa.decrypt(cipher, key)
+    if args.generate:
+        keys = rsa.generate_keys()
+
+    if out is not None:
+        print(out)
 
 
 if __name__ == "__main__":
--- src/rsa.py
+++ src/rsa.py
@@ -1,10 +1,10 @@
 
-def encrypt():
-    pass
+def encrypt(text, key):
+    return text,key
 
 
-def decrypt():
-    pass
+def decrypt(cipher, key):
+    return cipher, key
 
 
 def generate_keys():
