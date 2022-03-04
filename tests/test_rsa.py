b86446ee13e8b384c18914ec7bd620e72520dcf9
inanimateobject <mirko.vonbell@helsinki.fi>
2022-03-04 21:05:36 +0200

    added tests for rsamath

--- tests/test_rsa.py
+++ tests/test_rsa.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from tiraRSA.rsa import rsa_interface as tri
 from tiraRSA.rsa import rsamath as trm
 
@@ -57,5 +58,42 @@ class TestRsaInterface(unittest.TestCase):
 
 
 class TestRsaMath(unittest.TestCase):
-    def test_dummy(self):
-        pass
+    def setUp(self):
+        """
+        corrects has correct outputs generated with Wolfram Mathematica
+        """
+        with open("data/rsacorrect.json") as ansjson:
+            self.corrects = json.load(ansjson)
+
+        primes = {
+            7943145101710562767712512886836874625466408815303442724266795375607174087802774885055943208810348273071090648052930763205438901854415084861090395327969639,
+            9859683912861508896364108357973411205849725192465586046244521470804052302811503119260174928221790706599031626977590103111024736783439335250311108612188489}
+        self.keys = trm.generate_keys(primes)
+        self.n = self.keys[0]
+        self.e = self.keys[1]
+        self.d = self.keys[2]
+
+    def test_expected_n(self):
+        expected_n = 78316899976860329571515896513117298086189377964186071578745511139196297894088489928306239411137840118892272141348247278371312940441161242685855216648379155981542703836876000527700983069797506415533163800799964819459616861208268232818947326151326747724072671650469979683838768189176600969872736692404737285471
+        self.assertEqual(self.n, expected_n)
+
+    def test_expected_e(self):
+        expected_e = 65537
+        self.assertEqual(self.e, expected_e)
+
+    def test_expected_d(self):
+        expected_d = 15294243623508047790119263950487995171522876449557279660353303085509007409516632411422973360139274711409474724217549518474071877996188445679767428937459456489925799961176103576134764028172790542131316637511995214196030450037277579091143310098261723528934995194448983884863556821479433311459487385531743168505
+        self.assertEqual(self.d, expected_d)
+
+    def test_encrypt_correct_output(self):
+        data = 1234567890987654321
+        encdata = trm.encrypt(data, self.n, self.e)
+        self.assertEqual(encdata, self.corrects["encryption"])
+
+    def test_decrypt_correct_output(self):
+        data = self.corrects["encryption"]
+        decdata = trm.decrypt(data, self.n, self.d)
+        self.assertEqual(decdata, 1234567890987654321)
+
+
+
